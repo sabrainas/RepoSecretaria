6c9d07c1c04d57d7f2fe5095fed96d7bc9f9a4f2
sabrina mendonça <109186458+sabrainas@users.noreply.github.com>
2024-04-19 10:36:15 -0300

    subindo arquivo formulario alunos

---
+++ FormularioAlunos/FormularioAlunos/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using FormularioAlunos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FormularioAlunos.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Livre()
+        {
+            return View();
+        }
+
+        public IActionResult Escolar()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
